b104fa4494f667caad8ea82b60cbd75b328605f4
Laila Doudouh Test <test@example.com>
2025-09-04 11:17:13 +0200

    added method so that aws connectivity is initiated when artifact.provder is s3

--- src/xsoar_client/artifact_provider.py
+++ src/xsoar_client/artifact_provider.py
@@ -1,3 +1,4 @@
+from src.xsoar_client import artifact_provider
 from __future__ import annotations
 
 import boto3
@@ -23,11 +24,18 @@ class ArtifactProvider:
         self.boto3_session = boto3.session.Session()  # pyright: ignore  # noqa: PGH003
         self.s3 = self.boto3_session.resource("s3")
 
-    def test_connection(self) -> None:
+    def test_connection(self) -> bool:
+        if self.artifacts_repo == "S3":
+            return self._test_connection_aws()
+        return False
+
+
+    def _test_connection_aws(self) -> bool:
         # TODO: test AWS/S3 connectivity here and raise sensible exeptions
         try:
             bucket = self.s3.Bucket(self.s3_bucket_name) #lager en bucketresource object for bucket_name
             bucket.load()
+            return True
 
         except NoCredentialsError as ex:
             msg = "AWS credentials not found."
@@ -43,6 +51,7 @@ class ArtifactProvider:
             raise RuntimeError(msg) from ex
         except Exception as ex:
             print(f"An error occurred: {ex}")
+        return False
 
 
     def _is_available_s3(self, *, pack_id: str, pack_version: str) -> bool:
